cf060978c4f3f12637dac445a08be7bb415eeff3
datadote <datadote@gmail.com>
2021-03-17 23:02:25 -0700

    removed plotly

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 #import libraries
 import json
-import plotly
+#import plotly
 
 import numpy as np
 import pandas as pd
@@ -14,71 +14,9 @@ app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
 #default page of our web-app
-#@app.route('/')
-#def home():
-#    return render_template('index.html')
-
 @app.route('/')
 def home():
-    rng = pd.date_range('1/1/2011', periods=7500, freq='H')
-    ts = pd.Series(np.random.randn(len(rng)), index=rng)
-
-    w = 500
-    h = w
-
-    graphs = [
-        dict(
-            data=[
-                dict(
-                    x=[1, 2, 3],
-                    y=[10, 20, 30],
-                    type='scatter'
-                ),
-            ],
-            layout=dict(
-                width=w,
-                height=h,
-                title='first graph'
-            )
-        ),
-
-        dict(
-            data=[
-                dict(
-                    x=[1, 3, 5],
-                    y=[10, 50, 30],
-                    type='bar'
-                ),
-            ],
-            layout=dict(
-                width=w,
-                height=h,
-                title='second graph'
-            )
-        ),
-
-        dict(
-            data=[
-                dict(
-                    x=ts.index,  # Can use the pandas data structures directly
-                    y=ts
-                )
-            ]
-        )
-    ]
-
-    # Add "ids" to each of the graphs to pass up to the client
-    # for templating
-    ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
-
-    # Convert the figures to JSON
-    # PlotlyJSONEncoder appropriately converts pandas, datetime, etc
-    # objects to their JSON equivalents
-    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return render_template('index.html',
-                           ids=ids,
-                           graphJSON=graphJSON)
+    return render_template('index.html')
 
 #To use the predict button in our web-app
 @app.route('/predict',methods=['POST'])
